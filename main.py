6910a21151058a5cdb3fa73bb86b75091f4654d6
Евгений <86568169+fomindev@users.noreply.github.com>
2022-03-08 01:55:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,29 @@ def get_info_by_ip(ip='127.0.0.1'):
         response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
        
         data = {
-            'IP >': response.get('query'),
-            'INT PROV >': response.get('isp'),
-            'ORG >': response.get('org'),
-            'COUNTRY >': response.get('country'),
-            'REGION NAME >': response.get('regionName'),
-            'CITY >': response.get('city'),
-            'ZIP >': response.get('zip'),
-            'LAT >': response.get('lat'),
-            'LON >': response.get('lon'),
-        }
+            'Status': response.get('status'),
+            'Ip': response.get('query'),
+            'Country': response.get('country'),
+            'Country code': response.get('countryCode'),
+            'Region': response.get('region'),
+            'Region name': response.get('regionName'),
+            'City': response.get('city'),
+            'Zip': response.get('zip'),
+            'Time zone': response.get('timezone'),
+            'Isp': response.get('isp'),
+            'Org': response.get('org'),
+            'As': response.get('as'),
+            'lat ': response.get('lat'),
+            'Lon ': response.get('lon')            
+            }
         for k, v in data.items():
             print(f'{k} : {v}')
 
         time.sleep(1) # Тут идет задержка в 1 секунду
+        print("-" * 10)
         print(f'{author}') # обычная переменная из файла config.py
- 
+        print("-" * 10)
+
     except requests.exceptions.ConnectionError:
         print('ОШИБКА | Пожалуйста, проверьте ваше соединение') # Ошибка
       
